031ecd6caab7cab6f2c6e0f3c0d2bc716d5db840
Benfain1234 <86921300+Benfain1234@users.noreply.github.com>
2022-10-11 20:24:34 +0300

    Add files via upload

--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,10 @@ const shopController = require("../controllers/shop");
 const router = express.Router();
 
 router.get("/shop", shopController.shop);
-router.get("/pricefilter1", shopController.PriceFilter1);
-router.get("/pricefilter2", shopController.PriceFilter2);
-router.get("/pricefilter3", shopController.PriceFilter3);
-router.get("/pricefilter4", shopController.PriceFilter4);
-router.get("/pricefilter5", shopController.PriceFilter5);
+router.post("/pricefilter1", shopController.PriceFilter1);
+router.post("/pricefilter2", shopController.PriceFilter2);
+router.post("/pricefilter3", shopController.PriceFilter3);
+router.post("/pricefilter4", shopController.PriceFilter4);
+router.post("/pricefilter5", shopController.PriceFilter5);
 
 module.exports = router;
