b1baf61570cf8dcaddf157c4549d7afe152d9d2a
Benfain1234 <86921300+Benfain1234@users.noreply.github.com>
2022-10-12 11:12:30 +0300

    Add files via upload

--- models/shop.js
+++ models/shop.js
@@ -9,6 +9,48 @@ function PriceFilter(Min,Max){
   console.log(result);
   return result;
 }
+ function Filter(array){
+     let allProducts = products.getAllProducts();
+   if(!array){return allProducts};
+   let filtered = [];
+   if(array.pricearray){
+       while(array.pricearray.length>0){
+           let price = array.pricearray.splice(0,1);
+           allProducts.forEach(product => {
+               if(product.price>price[0]-100&&product.price<=price[0]){
+                   filtered.push(product);
+               }
+           })
 
 
-module.exports = { getShop ,PriceFilter};
+       }
+   }
+   if(array.colorarray){
+       while (array.colorarray.length>0){
+           let color = array.colorarray.splice(0,1);
+           allProducts.forEach(product =>{
+               if(product.color=== color[0]){
+                   let index = filtered.findIndex(p => p.color === color[0]&& p.id === product.id)
+                   console.log(index);
+                   if(index===-1){  filtered.push(product);}
+
+               }
+           })
+       }
+   }
+ if(array.sizearray){
+   while(array.sizearray.length>0){
+       let size = array.sizearray.splice(0,1);
+       allProducts.forEach(product =>{
+           if(product.size === size[0]){
+               let index = filtered.findIndex(p => p.size === size[0]&& p.id === product.id)
+               if(index===-1){filtered.push(product)}
+           }
+       })
+   }
+ }
+
+  return filtered;
+}
+
+module.exports = { getShop ,PriceFilter,Filter};
