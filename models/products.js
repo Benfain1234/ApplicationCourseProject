88460bdc193b0a295d43d2d6167b3139929bea12
Benfain1234 <86921300+Benfain1234@users.noreply.github.com>
2022-10-12 11:13:11 +0300

    Add files via upload

--- models/products.js
+++ models/products.js
@@ -1,11 +1,14 @@
 //const { all } = require("../routes/products");
 
 const products = [
-  {
+
+    {
     id: 1,
     title: "product1",
     desc: "shirt",
     price: "100",
+    color:  "black",
+      size: "M",
     src: "img/product-1.jpg",
   },
   {
@@ -13,6 +16,8 @@ const products = [
     title: "product2",
     desc: "dress",
     price: "101",
+    color: "white",
+    size: "S",
     src: "img/product-2.jpg",
   },
   {
@@ -20,6 +25,8 @@ const products = [
     title: "product3",
     desc: "t",
     price: "200",
+    color: "red",
+    size: "L",
     src: "img/product-3.jpg",
   },
   {
@@ -27,6 +34,8 @@ const products = [
     title: "product4",
     desc: "shoes",
     price: "450",
+    color: "blue",
+    size: "XL",
     src: "img/product-4.jpg",
   },
   {
@@ -34,13 +43,17 @@ const products = [
     title: "product5",
     desc: "pants",
     price: "220",
+    color: "green",
+    size: "L",
     src: "img/product-5.jpg",
   },
   {
     id: 6,
     title: "product6",
     desc: "underwear",
-    price: "540",
+    price: "32",
+    color: "blue",
+    size: "M",
     src: "img/product-6.jpg",
   },
   {
@@ -48,13 +61,17 @@ const products = [
     title: "product7",
     desc: "sunglasses",
     price: "262",
+    color:  "black",
+    size: "S",
     src: "img/product-7.jpg",
   },
   {
     id: 8,
     title: "product8",
     desc: "scarf",
-    price: "90",
+    price: "124",
+    color: "white",
+    size: "XS",
     src: "img/product-8.jpg",
   },
 ];
@@ -97,9 +114,54 @@ const categories = [
     src: "img/cat-6.jpg",
   },
 ];
+const price1product = [
+  {
+    id: 1,
+    title: "product1",
+    desc: "shirt",
+    price: "100",
+    src: "img/product-1.jpg",
+  }
+]
+ const price2product = [  {
+   id: 2,
+   title: "product2",
+   desc: "dress",
+   price: "101",
+   src: "img/product-2.jpg",
+ },
+   {
+   id: 3,
+   title: "product3",
+   desc: "t",
+   price: "200",
+   src: "img/product-3.jpg",}
+
+
+ ]
+const price3product = [   {
+  id: 3,
+  title: "product3",
+  desc: "t",
+  price: "200",
+  src: "img/product-3.jpg",}]
+const price4product = [  {
+  id: 6,
+  title: "product6",
+  desc: "underwear",
+  price: "320",
+  src: "img/product-6.jpg",
+}]
+const price5product = [  {
+  id: 4,
+  title: "product4",
+  desc: "shoes",
+  price: "450",
+  src: "img/product-4.jpg",
+}]
 
 function getAllProducts() {
   return products;
 }
 
-module.exports = { getAllProducts };
+module.exports = { getAllProducts ,price1product,price2product,price3product,price4product,price5product};
