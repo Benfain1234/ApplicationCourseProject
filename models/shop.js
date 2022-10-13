e5898f53b1f4e5914604e629bc91e49327c1fe60
Benfain1234 <86921300+Benfain1234@users.noreply.github.com>
2022-10-13 12:04:53 +0300

    Add files via upload

--- models/shop.js
+++ models/shop.js
@@ -4,11 +4,7 @@ function getShop() {
   return products.getAllProducts();
 }
 
-function PriceFilter(Min,Max){
-  const result = products.getAllProducts().filter(product => product.price>Min && product.price<Max);
-  console.log(result);
-  return result;
-}
+
  function Filter(array){
      let allProducts = products.getAllProducts();
    if(!array){return allProducts};
@@ -16,8 +12,14 @@ function PriceFilter(Min,Max){
    if(array.pricearray){
        while(array.pricearray.length>0){
            let price = array.pricearray.splice(0,1);
+           console.log(price[0]);
            allProducts.forEach(product => {
-               if(product.price>price[0]-100&&product.price<=price[0]){
+
+               if(product.price>0&&product.price<=100&&price[0]==100){
+                   filtered.push(product);
+               }
+
+               if(product.price>price[0]-100&&product.price<=price[0]&&price[0]!=100){
                    filtered.push(product);
                }
            })
@@ -53,4 +55,43 @@ function PriceFilter(Min,Max){
   return filtered;
 }
 
-module.exports = { getShop ,PriceFilter,Filter};
+function SearchProduct(array,value){
+ let result = Filter(array);
+    const final = result.filter(product => product.title.includes(value));
+
+ return final;
+}
+
+function SortByPrice(array){
+    let result = Filter(array);
+    result.sort((a,b)=> a.price-b.price )
+
+    return result;
+}
+
+function SortBySize(array){
+    let result = Filter(array);
+    result.sort((a,b)=> a.size_value-b.size_value )
+
+    return result;
+}
+
+function SortByColor(array){
+    let result = Filter(array);
+    result.sort((a,b)=> {
+        const color1 = a.color.toUpperCase(); // ignore upper and lowercase
+        const color2 = b.color.toUpperCase(); // ignore upper and lowercase
+        if (color1 < color2) {
+            return -1;
+        }
+        if (color1 > color2) {
+            return 1;
+        }
+
+
+        return 0;
+    } );
+
+    return result;
+}
+module.exports = { getShop ,Filter,SearchProduct,SortByPrice,SortBySize,SortByColor};
