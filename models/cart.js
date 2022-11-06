7a76521a6a974febc819c4b7bdf027494caa9cbe
Benfain1234 <86921300+Benfain1234@users.noreply.github.com>
2022-11-06 19:07:18 +0200

    Add files via upload

--- models/cart.js
+++ models/cart.js
@@ -1,4 +1,4 @@
-const products = require("./products");
+const products = require("../services/products");
 const cart = [];
 
 function getCart() {
@@ -6,20 +6,58 @@ function getCart() {
 }
 
 function addProduct(id) {
-  var p = products.getAllProducts().find((c) => c.id == id);
-  cart.push(p);
+  products.findProduct(id).then((e) => {
+    if (cart.findIndex((item) => item.equals(e)) !== -1) {
+      var prod = cart.find((c) => c.id == id);
+      prod.quantity = parseInt(prod.quantity) + 1;
+    } else {
+      e.quantity = 1;
+      cart.push(e);
+    }
+  });
+}
+
+function totalProds() {
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    total += parseInt(cart[i].quantity);
+  }
+  return total;
+}
+
+function decreaseProduct(id) {
+  var p = cart.find((c) => c.id == id);
+  if (p !== null && parseInt(p.quantity) > 1) {
+    p.quantity = parseInt(p.quantity) - 1;
+  } else if (p !== null) {
+    cart.splice(cart.indexOf(p), 1);
+  }
 }
 
 function removeProduct(id) {
-  cart.splice(cart.indexOf(cart.find((c) => c.id == id)), 1);
+  var p = cart.find((c) => c.id == id);
+  if (p !== null) cart.splice(cart.indexOf(p), 1);
 }
 
 function getTotal() {
   var total = 0;
   for (var i = 0; i < cart.length; i++) {
-    total += parseInt(cart[i].price);
+    total += parseInt(cart[i].price) * parseInt(cart[i].quantity);
   }
   return total;
 }
 
-module.exports = { getCart, addProduct, removeProduct, getTotal };
+function EmptyCart(){
+
+  cart.length = 0;
+
+}
+module.exports = {
+  getCart,
+  addProduct,
+  removeProduct,
+  decreaseProduct,
+  getTotal,
+  totalProds,
+  EmptyCart,
+};
