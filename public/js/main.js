6dc1717bbc144d1b8d1a134d4456a01f0753e072
Benfain1234 <86921300+Benfain1234@users.noreply.github.com>
2022-11-06 19:07:00 +0200

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,162 @@
+$(() => {
+  $.get("/isConnect").done(function (res) {
+    if (res) {
+      $("#login").remove();
+      $("#register").remove();
 
+    } else {
+      $("#logout").remove();
+      $("#dropdown").remove();
+      for (var j = 0; j < 6; j++) {
+        if ($("#shoplink").hasClass("cat-img")) {
+          $("#shoplink").removeAttr("href");
+        } else {
+          $("#shoplink").remove();
+        }
+      }
+      var cart = document.querySelectorAll("#addToCart");
+      for (var i = 0; i < cart.length; i++) {
+        $("#addToCart").remove();
+      }
+      $("#qlinkcart").remove();
+      $("#qlinkcheckout").remove();
+    }
+  });
 
+  var droppers = document.querySelectorAll("#dropProd");
+  for (var i = 0; i < droppers.length; i++) {
+    const id = droppers[i].parentElement.parentElement.id;
+    droppers[i].addEventListener("click", function () {
+      $.ajax({
+        url: `http://localhost:8080/dropProduct?id=${id}`,
+      }).done(function (obj) {
+        var list = document.getElementById(id).parentElement;
+        var prod = document.getElementById(id);
+        list.removeChild(prod);
+      });
+    });
+  }
+  var cart = document.querySelectorAll("#addToCart");
+  for (var i = 0; i < cart.length; i++) {
+    const id = cart[i].parentElement.parentElement.id;
+    cart[i].addEventListener("click", function () {
+      $.ajax({
+        url: `http://localhost:8080/addProduct?id=${id}`,
+      }).done(function (obj) {
+        $("#cartVal").html(`${obj}`);
+        for (let index = 0; index < obj.length; index++) {
+          const element = obj[index];
+          let template = $("#cart_template").html();
+          for (const key in element) {
+            template = template.replace("{" + key + "}", element[key]);
+          }
+        }
+      });
+    });
+  }
+});
 
+function removeClick(id) {
+  $.ajax({
+    url: `http://localhost:8080/removeProduct?id=${id}`,
+  }).done(function (obj) {
+    let prod = document.getElementById("prodList");
+    let elem = document.getElementById(id);
+    const currPrice = parseInt(
+      document.getElementById(`price${id}`).innerText.slice(1)
+    );
+    var numOfProds = document.getElementById(`num${id}`).value;
+    prod.removeChild(elem);
+    var subT = parseInt(document.getElementById("subTotal").innerText.slice(1));
+    var subTotal = subT - currPrice * parseInt(numOfProds);
+    var total = subTotal + 10;
+    $("#subTotal").html(`$${subTotal}`);
+    $("#total").html(`$${total}`);
+    var cartVal = parseInt(document.getElementById("cartVal").innerText);
+    $("#cartVal").html(`${cartVal - parseInt(numOfProds)}`);
+    numOfProds = 0;
+  });
+}
+function decreaseClick(id) {
+  $.ajax({
+    url: `http://localhost:8080/decreaseProduct?id=${id}`,
+  }).done(function (obj) {
+    let prod = document.getElementById("prodList");
+    let elem = document.getElementById(id);
+    const currPrice = parseInt(
+      document.getElementById(`price${id}`).innerText.slice(1)
+    );
+    var numOfProds = document.getElementById(`num${id}`).value;
+    console.log(numOfProds);
+    if (parseInt(numOfProds) == 0) {
+      prod.removeChild(elem);
+    } else {
+      numOfProds = parseInt(numOfProds) - 1;
+    }
+    var subT = parseInt(document.getElementById("subTotal").innerText.slice(1));
+    var subTotal = subT - currPrice;
+    var total = subTotal + 10;
+    $("#subTotal").html(`$${subTotal}`);
+    $("#total").html(`$${total}`);
+    var cartVal = parseInt(document.getElementById("cartVal").innerText);
+    $("#cartVal").html(`${cartVal - 1}`);
+  });
+}
 
+$(() => {
+  var s = io();
+  s.on("userCnt", (msg) => {
+    $("#users").html(`Users connected: ${msg}`);
+  });
+  var cartList = document.querySelectorAll("#removeProd");
+  for (var i = 0; i < cartList.length; i++) {
+    const id = cartList[i].parentElement.parentElement.id;
+    cartList[i].addEventListener("click", function () {
+      removeClick(id);
+    });
+  }
+});
+$(() => {
+  var cartList = document.querySelectorAll("#decreaseProd");
+  for (var i = 0; i < cartList.length; i++) {
+    const id = cartList[i].parentElement.parentElement.parentElement.id;
+    cartList[i].addEventListener("click", function () {
+      decreaseClick(id);
+    });
+  }
+});
 (function ($) {
+
+  $("#PlaceOrder").click(()=>{
+
+    if($("#paypal")[0].checked === false&&$("#directcheck")[0].checked === false && $("#banktransfer")[0].checked === false){
+      $("#CheckoutDetails").append("<h1>Choose One Of The Payment Methods</h1>")
+      setTimeout(()=>{
+        $("#CheckoutDetails h1").remove();
+      },5000)
+    }else {
+      $.get("/emptycart").done(function (res){
+        if(res){
+          $("#CheckoutDetails").html("<h1>Congratulation We Got Your Order And We Will Contact You As Soon As Possibole<h1>")
+        }else {
+          $("#CheckoutDetails").append("<h1 id='payment-error' >Sorry Your Cart Is Empty Add Items To Complete Your Order</h1>")
+          setTimeout(()=>{
+            $("#CheckoutDetails h1").remove();
+          },5000)
+        }
+      });
+    }
+
+
+  })
   "use strict";
   var filter = {
     pricearray: [],
     colorarray: [],
-    sizearray: []
+    sizearray: [],
   };
   // Dropdown on mouse hover
   $(document).ready(function () {
-
     function toggleNavbarMethod() {
       if ($(window).width() > 992) {
         $(".navbar .dropdown")
@@ -27,7 +172,6 @@
     }
     toggleNavbarMethod();
     $(window).resize(toggleNavbarMethod);
-
   });
 
   // Back to top button
@@ -42,55 +186,6 @@
     $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
     return false;
   });
-
-  // Vendor carousel
-  //   $(".vendor-carousel").owlCarousel({
-  //     loop: true,
-  //     margin: 29,
-  //     nav: false,
-  //     autoplay: true,
-  //     smartSpeed: 1000,
-  //     responsive: {
-  //       0: {
-  //         items: 2,
-  //       },
-  //       576: {
-  //         items: 3,
-  //       },
-  //       768: {
-  //         items: 4,
-  //       },
-  //       992: {
-  //         items: 5,
-  //       },
-  //       1200: {
-  //         items: 6,
-  //       },
-  //     },
-  //   });
-
-  //   // Related carousel
-  //   $(".related-carousel").owlCarousel({
-  //     loop: true,
-  //     margin: 29,
-  //     nav: false,
-  //     autoplay: true,
-  //     smartSpeed: 1000,
-  //     responsive: {
-  //       0: {
-  //         items: 1,
-  //       },
-  //       576: {
-  //         items: 2,
-  //       },
-  //       768: {
-  //         items: 3,
-  //       },
-  //       992: {
-  //         items: 4,
-  //       },
-  //     },
-  //   });
 
   // Product Quantity
   $(".quantity button").on("click", function () {
@@ -108,337 +203,84 @@
     button.parent().parent().find("input").val(newVal);
   });
 
-  $("#price-1").click(()=>{
-   let index = filter.pricearray.findIndex((number)=> number === 100)
-   if(index===-1){
-     filter.pricearray.push(100);
-   }else{
-     filter.pricearray.splice(index,index+1);
-   }
+  $(".dropdown-menu-right a:nth-of-type(1)").click(() => {
+    $("#filter").load("http://localhost:8080/sortbyprice", { filter });
+  });
+  $(".dropdown-menu-right a:nth-of-type(2)").click(() => {
+    $("#filter").load("http://localhost:8080/sortbysize", { filter });
+  });
+  $(".dropdown-menu-right a:nth-of-type(3)").click(() => {
+    $("#filter").load("http://localhost:8080/sortbycolor", { filter });
+  });
+  $("#tel-aviv").click(() => {
+    $("#map").load("/telaviv");
+    $("#weather").empty();
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://weatherapi-com.p.rapidapi.com/forecast.json?q=tel-aviv&days=3",
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "2fc7d7c819msh6602ac9ee5edd5dp104abfjsn84c7351d0387",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+      },
+    };
 
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1', {filter})
-  })
+    $.ajax(settings).done(function (response) {
+      var txt1 = $("<p></p>").text(
+        "Weather at Tel-Aviv :" + response.current.condition.text
+      );
+      $("#weather").html(txt1);
+      var txt2 = $("<p></p>").text("Temp (F) : " + response.current.temp_f);
+      $("#weather").append(txt2);
+    });
+  });
+  $("#bat-yam").click(() => {
+    $("#map").load("/batyam");
+    $("#weather").empty();
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://weatherapi-com.p.rapidapi.com/forecast.json?q=bat%20yam&days=3",
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "2fc7d7c819msh6602ac9ee5edd5dp104abfjsn84c7351d0387",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+      },
+    };
 
-  $("#price-2").click(()=>{
-    let index = filter.pricearray.findIndex((number)=> number === 200)
-    if(index===-1){
-      filter.pricearray.push(200);
-    }else{
-      filter.pricearray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
+    $.ajax(settings).done(function (response) {
+      var txt3 = $("<p></p>").text(
+        "Weather at Bat-Yam :" + response.current.condition.text
+      );
+      $("#weather").html(txt3);
+      var txt4 = $("<p></p>").text(
+        "Local time : " + response.location.localtime
+      );
+      $("#weather").append(txt4);
+    });
+  });
+  $("#rishon-lezion").click(() => {
+    $("#map").load("/rishon");
+    $("#weather").empty();
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url: "https://weatherapi-com.p.rapidapi.com/forecast.json?q=rishon%20lezion&days=3",
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "2fc7d7c819msh6602ac9ee5edd5dp104abfjsn84c7351d0387",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+      },
+    };
 
-  $("#price-3").click(()=>{
-    let index = filter.pricearray.findIndex((number)=> number === 300)
-    if(index===-1){
-      filter.pricearray.push(300);
-    }else{
-      filter.pricearray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $("#price-4").click(()=>{
-    let index = filter.pricearray.findIndex((number)=> number === 400)
-    if(index===-1){
-      filter.pricearray.push(400);
-    }else{
-      filter.pricearray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $("#price-5").click(()=>{
-    let index = filter.pricearray.findIndex((number)=> number === 500)
-    if(index===-1){
-      filter.pricearray.push(500);
-    }else{
-      filter.pricearray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $("#color-1").click(()=>{
-    let index = filter.colorarray.findIndex((color)=> color === "black")
-    if(index===-1){
-      filter.colorarray.push("black");
-    }else{
-      filter.colorarray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-  $("#color-2").click(()=>{
-    let index = filter.colorarray.findIndex((color)=> color === "white")
-    if(index===-1){
-      filter.colorarray.push("white");
-    }else{
-      filter.colorarray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-  $("#color-3").click(()=>{
-    let index = filter.colorarray.findIndex((color)=> color === "red")
-    if(index===-1){
-      filter.colorarray.push("red");
-    }else{
-      filter.colorarray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $("#color-4").click(()=>{
-    let index = filter.colorarray.findIndex((color)=> color === "blue")
-    if(index===-1){
-      filter.colorarray.push("blue");
-    }else{
-      filter.colorarray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $("#color-5").click(()=>{
-    let index = filter.colorarray.findIndex((color)=> color === "green")
-    if(index===-1){
-      filter.colorarray.push("green");
-    }else{
-      filter.colorarray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $("#size-1").click(()=>{
-    let index = filter.sizearray.findIndex((color)=> color === "XS")
-    if(index===-1){
-      filter.sizearray.push("XS");
-    }else{
-      filter.sizearray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $("#size-2").click(()=>{
-    let index = filter.sizearray.findIndex((color)=> color === "S")
-    if(index===-1){
-      filter.sizearray.push("S");
-    }else{
-      filter.sizearray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $("#size-3").click(()=>{
-    let index = filter.sizearray.findIndex((color)=> color === "M")
-    if(index===-1){
-      filter.sizearray.push("M");
-    }else{
-      filter.sizearray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $("#size-4").click(()=>{
-    let index = filter.sizearray.findIndex((color)=> color === "L")
-    if(index===-1){
-      filter.sizearray.push("L");
-    }else{
-      filter.sizearray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $("#size-5").click(()=>{
-
-    let index = filter.sizearray.findIndex((color)=> color === "XL")
-    if(index===-1){
-      filter.sizearray.push("XL");
-    }else{
-      filter.sizearray.splice(index,index+1);
-    }
-    console.log(filter);
-    $("#filter").load('http://localhost:8080/pricefilter1',{filter})
-  })
-
-  $(".search_product_button2").click(()=>{
-    const value =  $(".input2").val();
-    const filter2 = filter;
-    if($("#price-1")[0].checked  === true) {
-      $("#price-1").click();
-    }
-    if($("#price-2")[0].checked  === true) {
-      $("#price-2").click();
-    }
-    if($("#price-3")[0].checked  === true) {
-      $("#price-3").click();
-    }
-    if($("#price-4")[0].checked  === true) {
-      $("#price-4").click();
-    }
-    if($("#price-5")[0].checked  === true) {
-      $("#price-5").click();
-    }
-    if($("#color-1")[0].checked  === true) {
-      $("#color-1").click();
-    }
-    if($("#color-2")[0].checked  === true) {
-      $("#color-2").click();
-    }
-    if($("#color-3")[0].checked  === true) {
-      $("#color-3").click();
-    }
-    if($("#color-4")[0].checked  === true) {
-      $("#color-4").click();
-    }
-    if($("#color-5")[0].checked  === true) {
-      $("#color-5").click();
-    }
-    if($("#size-1")[0].checked  === true) {
-      $("#size-1").click();
-    }
-    if($("#size-2")[0].checked  === true) {
-      $("#size-2").click();
-    }
-    if($("#size-3")[0].checked  === true) {
-      $("#size-3").click();
-    }
-    if($("#size-4")[0].checked  === true) {
-      $("#size-4").click();
-    }
-    if($("#size-5")[0].checked  === true) {
-      $("#size-5").click();
-    }
-    $("#filter").load('http://localhost:8080/searchproduct',{filter2,value})
-
-
-  })
-
-  $(".dropdown-menu-right a:nth-of-type(1)").click(()=>{
-    $("#filter").load('http://localhost:8080/sortbyprice',{filter})
-  })
-  $(".dropdown-menu-right a:nth-of-type(2)").click(()=>{
-    $("#filter").load('http://localhost:8080/sortbysize',{filter})
-  })
-  $(".dropdown-menu-right a:nth-of-type(3)").click(()=>{
-    $("#filter").load('http://localhost:8080/sortbycolor',{filter})
-  })
+    $.ajax(settings).done(function (response) {
+      var txt1 = $("<p></p>").text(
+        "Weather at Rishon-Lezion :" + response.current.condition.text
+      );
+      $("#weather").html(txt1);
+      var txt2 = $("<p></p>").text("Temp (C) : " + response.current.temp_c);
+      $("#weather").append(txt2);
+    });
+  });
 })(jQuery);
-=======
-(function ($) {
-    "use strict";
-    
-    // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-    // Vendor carousel
-    $('.vendor-carousel').owlCarousel({
-        loop: true,
-        margin: 29,
-        nav: false,
-        autoplay: true,
-        smartSpeed: 1000,
-        responsive: {
-            0:{
-                items:2
-            },
-            576:{
-                items:3
-            },
-            768:{
-                items:4
-            },
-            992:{
-                items:5
-            },
-            1200:{
-                items:6
-            }
-        }
-    });
-
-
-    // Related carousel
-    $('.related-carousel').owlCarousel({
-        loop: true,
-        margin: 29,
-        nav: false,
-        autoplay: true,
-        smartSpeed: 1000,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            992:{
-                items:4
-            }
-        }
-    });
-
-
-    // Product Quantity
-    $('.quantity button').on('click', function () {
-        var button = $(this);
-        var oldValue = button.parent().parent().find('input').val();
-        if (button.hasClass('btn-plus')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        button.parent().parent().find('input').val(newVal);
-    });
-    
-})(jQuery);
-
-
---
+++ public/js/shop.js
@@ -0,0 +1,197 @@
+$(() => {
+  $("#filter").on("click", "#addToCart", function (e) {
+    const id = e.currentTarget.parentElement.parentElement.id;
+    $.ajax({
+      url: `http://localhost:8080/addProduct?id=${id}`,
+    }).done(function (obj) {
+      $("#cartVal").html(`${obj}`);
+      for (let index = 0; index < obj.length; index++) {
+        const element = obj[index];
+        let template = $("#cart_template").html();
+        for (const key in element) {
+          template = template.replace("{" + key + "}", element[key]);
+        }
+      }
+    });
+  });
+
+  $("#sub").click(function (e) {
+    const v = document.getElementById("forms").elements;
+    const url = `http://localhost:8080/updateProduct?id=${v.id.value}&&title=${v.title.value}&&desc=${v.desc.value}
+    &&price=${v.price.value}&&color=${v.color.value}&&size=${v.size.value}&&quantity=${v.quantity.value}&&
+    src=${v.src.value}`;
+    $.ajax({
+      url: url,
+    }).done(function (obj) {
+      $("#filter").html("");
+      for (let index = 0; index < obj.length; index++) {
+        const element = obj[index];
+        let template = $("#price_template").html();
+        for (const key in element) {
+          template = template.replace("{" + key + "}", element[key]);
+        }
+        $("#filter").append(template);
+      }
+    });
+  });
+
+  document.getElementById("priceSort").addEventListener("click", function () {
+    $.ajax({
+      url: `http://localhost:8080/sortByPrice`,
+    }).done(function (obj) {
+      $("#filter").html("");
+      for (let index = 0; index < obj.length; index++) {
+        const element = obj[index];
+        let template = $("#price_template").html();
+        for (const key in element) {
+          template = template.replace("{" + key + "}", element[key]);
+        }
+        $("#filter").append(template);
+      }
+    });
+  });
+
+  function clickedFilter(priceArr, colorArr, sizeArr, name) {
+    var urlArr = [];
+    var colorNames = ["black", "white", "red", "blue", "green"];
+    var sizeNames = ["XS", "S", "M", "L", "XL"];
+    var checkedSizes = [];
+    var checkedColors = [];
+    var checkCounter = 0;
+    for (var i = 0; i < colorArr.length; i++) {
+      if (colorArr[i].checked) {
+        checkedColors.push(colorNames[i]);
+      }
+      if (sizeArr[i].checked) {
+        checkedSizes.push(sizeNames[i]);
+      }
+    }
+    for (var i = 0; i < priceArr.length; i++) {
+      if (priceArr[i].checked) {
+        checkCounter++;
+        var min = (parseInt(priceArr[i].id.split("-")[1]) - 1) * 100;
+        var max = parseInt(priceArr[i].id.split("-")[1]) * 100 - 1;
+        var tempURL = `http://localhost:8080/pricefilter1?min=${min}&&max=${max}&&color[]=${checkedColors}&&size[]=${checkedSizes}&&name=${name}`;
+        urlArr.push(tempURL);
+      }
+    }
+    if (checkCounter > 0) {
+      $("#filter").html("");
+      for (var i = 0; i < urlArr.length; i++) {
+        $.ajax({
+          url: urlArr[i],
+        }).done(function (obj) {
+          for (var j = 0; j < obj.length; j++) {
+            const element = obj[j];
+            let template = $("#price_template").html();
+            for (const key in element) {
+              template = template.replace(`{${key}}`, element[key]);
+            }
+            $("#filter").append(template);
+          }
+        });
+      }
+    } else {
+      var url = `http://localhost:8080/pricefilter1?min=0&&max=1000000000&&color[]=${checkedColors}&&size[]=${checkedSizes}&&name=${name}`;
+      $.ajax({
+        url: url,
+      }).done(function (obj) {
+        $("#filter").html("");
+        for (var j = 0; j < obj.length; j++) {
+          const element = obj[j];
+          let template = $("#price_template").html();
+          for (const key in element) {
+            template = template.replace(`{${key}}`, element[key]);
+          }
+          $("#filter").append(template);
+        }
+      });
+    }
+  }
+  var prices = [];
+  var colors = [];
+  var sizes = [];
+  prices.push(document.getElementById("price-1"));
+  prices.push(document.getElementById("price-2"));
+  prices.push(document.getElementById("price-3"));
+  prices.push(document.getElementById("price-4"));
+  prices.push(document.getElementById("price-5"));
+  colors.push(document.getElementById("color-1"));
+  colors.push(document.getElementById("color-2"));
+  colors.push(document.getElementById("color-3"));
+  colors.push(document.getElementById("color-4"));
+  colors.push(document.getElementById("color-5"));
+  sizes.push(document.getElementById("size-1"));
+  sizes.push(document.getElementById("size-2"));
+  sizes.push(document.getElementById("size-3"));
+  sizes.push(document.getElementById("size-4"));
+  sizes.push(document.getElementById("size-5"));
+  $("#inp_search").keyup(() => {
+    clickedFilter(prices, colors, sizes, $("#inp_search").val());
+  });
+  for (var i = 0; i < prices.length; i++) {
+    prices[i].addEventListener("click", function () {
+      clickedFilter(prices, colors, sizes, $("#inp_search").val());
+    });
+    colors[i].addEventListener("click", function () {
+      clickedFilter(prices, colors, sizes, $("#inp_search").val());
+    });
+    sizes[i].addEventListener("click", function () {
+      clickedFilter(prices, colors, sizes, $("#inp_search").val());
+    });
+  }
+});
+
+$(".search_product_button2").click(() => {
+  const value = $(".input2").val();
+  const filter2 = filter;
+  if ($("#price-1")[0].checked === true) {
+    $("#price-1").click();
+  }
+  if ($("#price-2")[0].checked === true) {
+    $("#price-2").click();
+  }
+  if ($("#price-3")[0].checked === true) {
+    $("#price-3").click();
+  }
+  if ($("#price-4")[0].checked === true) {
+    $("#price-4").click();
+  }
+  if ($("#price-5")[0].checked === true) {
+    $("#price-5").click();
+  }
+  if ($("#color-1")[0].checked === true) {
+    $("#color-1").click();
+  }
+  if ($("#color-2")[0].checked === true) {
+    $("#color-2").click();
+  }
+  if ($("#color-3")[0].checked === true) {
+    $("#color-3").click();
+  }
+  if ($("#color-4")[0].checked === true) {
+    $("#color-4").click();
+  }
+  if ($("#color-5")[0].checked === true) {
+    $("#color-5").click();
+  }
+  if ($("#size-1")[0].checked === true) {
+    $("#size-1").click();
+  }
+  if ($("#size-2")[0].checked === true) {
+    $("#size-2").click();
+  }
+  if ($("#size-3")[0].checked === true) {
+    $("#size-3").click();
+  }
+  if ($("#size-4")[0].checked === true) {
+    $("#size-4").click();
+  }
+  if ($("#size-5")[0].checked === true) {
+    $("#size-5").click();
+  }
+  $("#filter").load("http://localhost:8080/searchproduct", {
+    filter2,
+    value,
+  });
+});
